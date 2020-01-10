e4e4562aea3031a8b925a595c40b733683b952e8
wuxichen <wuxichen@shzhanmeng.com>
2020-01-10 18:09:39 +0800

    react

--- wuxichen/src/components/UserList.js
+++ wuxichen/src/components/UserList.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 export default function(props){
+    let[users,setUser] = useState 
     return (
         <div>UserList</div>
     )
